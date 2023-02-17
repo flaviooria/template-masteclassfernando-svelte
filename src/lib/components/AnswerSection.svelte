8b20b554964b435c766df29b25c425b0960b133a
Flavio Oria <flavio.oriap@gmail.com>
2023-02-17 08:57:54 +0100

    Add components

---
+++ src/lib/components/AnswerSection.svelte
@@ -0,0 +1,68 @@
+<script>
+	export let category = '';
+	
+	let listCategories = [];
+
+	if (category.includes(',')) {
+		listCategories = category.split(',');
+	} else {
+		listCategories.push(category);
+	}
+</script>
+
+<main class="my-3 px-3 grid grid-cols-3">
+	<article class="col-span-2">
+		<h3 class="text-2xl text-white font-bold mb-6 ml-3">Respuestas recientes</h3>
+		<ol class="border-l-2 border-primary">
+			<li>
+				<div class="md:flex flex-start">
+					<div class="bg-primary w-6 h-6 flex items-center justify-center rounded-full -ml-3">
+						<div>
+							<img
+								src="https://ui-avatars.com/api/?background=BACFA9&name=Flavio+Oria&rounded=true"
+								alt="avatar"
+							/>
+						</div>
+					</div>
+					<div class="block p-6 rounded-lg shadow-lg bg-gray-100 max-w-md ml-6 mb-10 w-full">
+						<div class="flex justify-between mb-4">
+							<p class="font-medium text-primary text-sm">Título</p>
+							<p class="font-medium text-primary text-sm">Fecha: 04/02/2022</p>
+						</div>
+						<p class="text-gray-700 mb-6">Contenido</p>
+						<button
+							type="button"
+							class="inline-block px-4 py-1.5 bg-terniary text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-primary hover:shadow-lg focus:bg-primary focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+							data-mdb-ripple="true">Mas contenido</button
+						>
+						<button
+							type="button"
+							class="inline-block px-3.5 py-1 border-2 border-primary text-primary font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+							data-mdb-ripple="true">See demo</button
+						>
+					</div>
+				</div>
+			</li>
+		</ol>
+	</article>
+	<aside class="col-span-1">
+		<h1 class="text-2xl text-white font-bold mb-6">Categorías</h1>
+		<div class="my-6">
+			<ul class="bg-white rounded-lg border border-primary  text-primary">
+				{#each listCategories as category, index}
+					{#if index != listCategories.length - 1}
+						<li class="p-3 border-b border-primary w-full rounded-t-lg">
+							{category}
+						</li>
+					{:else}
+						<li class="p-3 w-full rounded-t-lg">
+							{category}
+						</li>
+					{/if}
+					
+				{/each}
+			</ul>
+		</div>
+	</aside>
+</main>
+
---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,33 @@
+<script>
+    // Recibimos los datos del form
+    export let form;
+	export let nameButton = '';
+	export let width = 'w-2/3';
+	console.log({form});
+
+    let showPassword = false;
+    const handleShowPassword = () => (showPassword = !showPassword);
+</script>
+
+<form class="flex flex-col {width}" method="post">
+	<label class="text-white text-xl font-medium my-3" for="name">Nombre:</label>
+	<input class="p-3 rounded-sm text-primary text-lg" type="text" name="name" id="name" placeholder="tú nombre" value={form?.name ?? ''}/>
+	<label class="text-white text-xl font-medium my-3" for="surname">Apellido:</label>
+	<input class="p-3 rounded-sm text-primary text-lg" type="text" name="surname" id="surname" placeholder="tú apellido" value={form?.surname ?? ''}/>
+	<label class="text-white text-xl font-medium my-3" for="email">Correo electronico:</label>
+	<input class="p-3 rounded-sm text-primary text-lg" type="text" name="email" id="email" placeholder="example@fernando.com" value={form?.email ?? ''} />
+	<label class="text-white text-xl font-medium mt-3" for="password">Contraseña</label>
+	<div class="flex items-center my-3">
+		<input class="p-3 rounded-sm text-primary text-lg mr-8 w-full" type={showPassword ? 'text' : 'password'} name="password" id="password" placeholder="contraseña" value={form?.password ?? ''} />
+		<button on:click={handleShowPassword} type="button">
+			<img src="/images/invisible.png" alt="hide_password">
+		</button>
+	</div>
+	{#if form?.invalid}
+		<p class="text-orange-300 mt-1">{form?.message}</p>
+	{/if}
+	<button class="bg-white p-3 rounded-sm text-primary text-lg hover:bg-terniary hover:text-white hover:transition-all hover:ease-in hover:duration-700 duration-300 mt-3" type="submit">
+		{nameButton}
+	</button>
+</form>
+
